b096e2629cd46da00606adb094780c63fd99999d
Lis <lp598@kent.ac.uk>
2026-02-02 13:18:29 +0000

    camel to snake

--- camel.py
+++ camel.py
@@ -1,1 +1,8 @@
-# Enter your code here
+Input = input("Write in camel here:")
+
+word = ''.join(['_' + char.lower() if char.isupper() else char for char in Input])
+
+if word.startswith("_"):
+    word = word[1:]
+
+print(word)
