c680bf1a2611740133ac3ebd7945d479e00c9c52
Lis <lp598@kent.ac.uk>
2026-02-02 14:21:43 +0000

    coffee

--- coffee.py
+++ coffee.py
@@ -1,1 +1,16 @@
-#Your code goes here
+price = 75
+moneyList = [5,10,20,50]
+
+while price > 0:
+    print(f"price: {price}p")
+    coin = int(input("insert coin:"))
+
+    if coin in moneyList:
+        price -= coin
+    else:
+        print("insert valid coin")
+
+print("thank you, dispensing coffee")
+
+change = abs(price)
+print(f"change owed: {change}p")
