efa704bc1ae8d1a4f76c534f1034fa0461d6664f
Lis <lp598@kent.ac.uk>
2026-02-02 14:32:08 +0000

    explination of code

--- coffee.py
+++ coffee.py
@@ -1,16 +1,18 @@
-price = 75
-moneyList = [5,10,20,50]
+#**"statement of requirements"**
 
-while price > 0:
+price = 75 # The total price of the drink
+moneyList = [5,10,20,50] # The list of coins the machine will accept
+
+while price > 0: 
     print(f"price: {price}p")
-    coin = int(input("insert coin:"))
+    coin = int(input("insert coin:")) # While the price is higher then 0 the code will prompt user to insert more coins
 
     if coin in moneyList:
-        price -= coin
+        price -= coin # If the coin can be accepted it will subtract from the total
     else:
-        print("insert valid coin")
+        print("insert valid coin") # If the coin cant be accepted the user will be prompted to insert a valid coin
 
 print("thank you, dispensing coffee")
 
-change = abs(price)
-print(f"change owed: {change}p")
+change = abs(price) 
+print(f"change owed: {change}p") # The machine at the end of the while loop will print the value of anything passed 0
